41c45e6b7bfd032a072d667487153c993b9b229a
Ray Turetta <rayturetta@gmail.com>
2024-12-18 17:53:53 -0300

    Alteração na model Report.

--- backend/apps/reports/models.py
+++ backend/apps/reports/models.py
@@ -11,9 +11,9 @@ class Report (models.Model):
     institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
     company =  models.ForeignKey(Company, on_delete=models.CASCADE)
 
-    #activity = 
-    #activity_change =
-    #society_change = 
+    #ide_activity = 
+    #ide_activity_change =
+    #ide_society_change = 
 
     #adm_documents = 
     #adm_management = 
@@ -21,8 +21,28 @@ class Report (models.Model):
     #adm_decision_process = 
     #adm_powers = 
     #adm_succession_process = 
-    #adm_other =  
-
+    #adm_other = 
+    
+    #fac
+    #inf
+    #loc
+    #tal
+    #pro
+    #pss
+    #com
+    #sto
+    #sup
+    #cus
+    #mkt
+    #inv
+    #fin
+    #ctr
+    #peo
+    #ite
+    #esg
+    #res
+    #tre
+    
     #opinion = 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
